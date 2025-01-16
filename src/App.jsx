b307b346a08a544cf2658f422bcb77d301ae6d04
DeexantBharti <deexantbharti4321@gmail.com>
2025-01-16 15:46:06 +0530

    Header component created

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,18 @@ import SignIn from './pages/SignIn.jsx';
 import SignUp from './pages/SignUp.jsx';
 import Profile from './pages/Profile.jsx';
 import About from './pages/About.jsx';
-
+import Header from './components/Header.jsx';
 export default function App() {
   return <BrowserRouter>
+  <Header />
   <Routes>
     <Route path="/" element = {<Home/>}/>
     <Route path='/sign-in' element = {<SignIn/>} />
     <Route path='/sign-up' element = {<SignUp />} />
     <Route path='/about' element = {<About />} />
     <Route path='/profile' element = {<Profile />} />
+   
   </Routes>
+  
   </BrowserRouter>
 }
