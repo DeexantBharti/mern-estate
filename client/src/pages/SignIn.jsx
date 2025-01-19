6b27684e5e033555297e181fbf939af9ca1a6989
DeexantBharti <deexantbharti4321@gmail.com>
2025-01-19 19:33:40 +0530

    add redux toolkit.

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,16 @@
 
 import { useState } from 'react';
 import { Link,useNavigate } from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import { signInStart, signInFailure, signInSuccess } from '../redux/user/userSlice.js';
+
 
 
 export default function SignIn() {
   const[formData, setFormData] = useState({});
-  const[error,setError] = useState(null);
-  const[loading , setLoading] = useState(false);
+  const {loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
       setFormData(
         {
@@ -21,6 +24,8 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
   try {
+    // setLoading(true); 
+    dispatch(signInStart());
     const res = await fetch('/api/auth/signin',{
       method:'POST',
       headers:{
@@ -30,17 +35,15 @@ export default function SignIn() {
     });
     const data = await res.json();
     if(data.success === false){
-      setLoading(false);
-      setError(data.message);
+      dispatch(signInFailure(data.message));
       
       return;
     }
-    setLoading(false);
-    setError(null);
+    
+    dispatch(signInSuccess(data));
     navigate('/');
   } catch (error) {
-    setLoading(false);
-    setError(error.message);
+   dispatch(signInFailure(error.message));
   }
   };
   
@@ -56,7 +59,7 @@ export default function SignIn() {
       <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading?'Loading...' : 'Sign In'}</button>
     </form>
     <div className='flex gap-2 mt-5'>
-    <p>Don't have an account?</p>
+    <p>Do not have an account?</p>
     <Link to={"/sign-in"}>
     <span className='text-blue-700 hover:text-red-700 hover:font-semibold'>Sign Up</span>
     </Link>
